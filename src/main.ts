0a1baf3fd2c49f7d1cc80d3c5168a8dec0cb18a9
ga84gor <ga84gor@forstudents.onmicrosoft.com>
2019-07-30 21:24:46 +0200

    ElectronJS Dummy with Babel, chai+mocha and TS

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var electron_1 = require("electron");
+
+function createWindow() {
+  // Create the browser window.
+  var win = new electron_1.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  }); // and load the index.html of the app.
+
+  win.loadFile('index.html');
+}
+
+electron_1.app.on('ready', createWindow);
