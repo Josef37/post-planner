456fcbd28f2a79a8c74c1ce0a1b6ffd259a62f4e
ga84gor <ga84gor@forstudents.onmicrosoft.com>
2019-08-09 19:31:14 +0200

    Documented startup script

--- src/script.ts
+++ src/script.ts
@@ -5,6 +5,11 @@ import { Controller } from "./controller";
 import { readFileSync } from "fs";
 import { Persistence } from "./persistence";
 
+/**
+ * Loads the default posts from a file and puts them into a post list.
+ * Then creates an empty account list.
+ * @param path the path of the posts file (JSON object with property "posts", which is a list of "title" and "url")
+ */
 function loadPosts(path = './data/posts.json'): AccountList {
     const { posts: parsed }: { posts: { title: string; url: string }[] } 
         = JSON.parse(readFileSync(path, 'utf-8'));
@@ -18,6 +23,10 @@ function loadPosts(path = './data/posts.json'): AccountList {
     return new AccountList([], [postList]);
 }
 
+/*
+ * Create a controller from the latest snapshot.
+ * If no snapshot is found, load the default posts.
+ */
 try {
     new Controller(Persistence.load());
 } catch(error) {
